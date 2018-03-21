d9fe888bd1a6d8bc17eedc27c6d1483065cc8710
doncurt <donovan.adams@students.makeschool.com>
2018-03-20 23:58:06 -0700

    almost done with more of this

--- project/callroutingproject.py
+++ project/callroutingproject.py
@@ -2,12 +2,32 @@ import sys
 sys.path.append('../source')
 from hashtable import HashTable
 class CallRouting(object):
-    def __init__(self,telenum):
+    def __init__(self,number,route):
         '''takes in a phone number and spits out the cost to run it'''
-        # the dictionary of comparisions for the first scenario
-        self.route_costs = {'+86153':0.84, '+449275049':0.49, '+8130':0.68,'+4928843955':0.40 ,'+449187847':0.48,'8197753':0.75, '+449916707':0.58, '+64655676':0.40,'+34924199':0.39,'+1941613':0.05}
-        #telephin number to be checked
-        self.telenum = telenum
+        #route costs from file to be read
+        filename= route
+        #declar empty hast table to store values
+        cost_hash_table =HashTable()
+        with open(filename) as file_object:
+            #for everyline in the file
+            for line in file_object:
+                #strip it of whitespace
+                line =line.strip()
+                #split it into a string of two
+                line=line.split(',')
+                #set the ket to the firs in the list and the value to the second( turned into a float)
+                cost_hash_table.set(line[0],float(line[1]))
+
+        self.route_costs = cost_hash_table
+        #telephone numbers from file to be read
+        filename = number
+        phone_numbers =[]
+        with open(filename) as file_object:
+            for line in file_object:
+                line =line.strip()
+                phone_numbers.append(line)
+
+        self.telenum = phone_numbers
 
     def cost_to_route(self):
         'Based on the numbert thats passed in it loops through the dictionaries keys and maatches t the values, which is the cost'
@@ -19,11 +39,4 @@ class CallRouting(object):
 if __name__ == '__main__':
     # call = CallRouting('+86153')
     # print(call.cost_to_route())
-    filename='data/call-costs-3.txt'
-    ht=HashTable()
-    with open(filename) as file_object:
-        for line in file_object:
-            line =line.strip()
-            line=line.split(',')
-            ht.set(line[0],float(line[1]))
-    print(ht)
+    
