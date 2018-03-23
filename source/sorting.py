cbe1edcf3c4c87cafa09bb26b6408ba89033c8ad
doncurt <donovan.adams@students.makeschool.com>
2018-03-23 09:12:52 -0700

    bingo

--- source/sorting.py
+++ source/sorting.py
@@ -6,13 +6,18 @@ def is_sorted(items):
     TODO: Running time: on due to having to inerate through all of the items
     TODO: Memory usage: ??? Why and under what conditions?"""
     # TODO: Check that all adjacent items are in order, return early if not
-    if len(items)==1:
+    #check if the list length is 0 or 1, if empty
+    # then technically tis sorted, if theres one item in the list, same
+    if (len(items) == 0 or len(items) == 1):
         return True
-    for i in range(len(items)):
-        if items[i] < items[i+1]:
-            return items[1:]
-        else:
+    #inerate through the list based on positions untill the end of th list
+    for i in range(1, len(items)):
+        #if the last item is larger than the current one we knwo it isntsorted
+        if (items[i-1] > items[i]):
+
             return False
+    #else return true
+    return True
 
 def bubble_sort(items):
     """Sort given items by swapping adjacent items that are out of order, and
